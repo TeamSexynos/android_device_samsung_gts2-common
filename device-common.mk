d1461ff4d7b620e0ccd8c0fdf7c19957a9ea7089
Lunarixus <Nathanb644@hotmail.co.uk>
2020-07-14 22:19:41 +0000

    gts2-common: Add ueventd from trelte-common
    This is a common file and is very much needed since it contains the permissions
    for camera, fingerprint, audio etc.
    
    Signed-off-by: Lunarixus <Nathanb644@hotmail.co.uk>

--- device-common.mk
+++ device-common.mk
@@ -247,6 +247,7 @@ PRODUCT_PACKAGES += \
     init.universal5433.rc \
     init.universal5433.usb.rc \
     init.samsung.rc \
+    ueventd.universal5433.rc \
     init.wifi.rc \
     init.power.rc \
     sswap
